3137638c9f5b69a68240e53a4f4cb253ce753f2e
Martin LinuxVBox <marting12@live.dk>
2016-04-11 21:02:01 +0200

    Adds mutual friendships

---
+++ app/models/friend_request.rb
@@ -0,0 +1,25 @@
+class FriendRequest < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+	validates :user, presence: true
+	validates :friend, presence: true, uniqueness: { scope: :user }
+
+	validate :not_friends
+  validate :not_pending
+
+  def accept
+    user.friends << friend
+    destroy
+  end
+
+  private
+
+  def not_friends
+    errors.add(:friend, 'is already added') if user.friends.include?(friend)
+  end
+
+  def not_pending
+    errors.add(:friend, 'already requested friendship') if friend.pending_friends.include?(user)
+  end
+end
