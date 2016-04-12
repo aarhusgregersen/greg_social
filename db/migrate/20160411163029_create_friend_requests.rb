8984b0e03d6b90c565ef504573a1e020160a4f60
Martin LinuxVBox <marting12@live.dk>
2016-04-12 04:43:59 +0200

    Fixes mutual friendships schema

--- db/migrate/20160411163029_create_friend_requests.rb
+++ db/migrate/20160411163029_create_friend_requests.rb
@@ -2,7 +2,7 @@ class CreateFriendRequests < ActiveRecord::Migration
   def change
     create_table :friend_requests do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :friend, index: true #, foreign_key: true
 
       t.timestamps null: false
     end
