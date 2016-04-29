3f3525dc17348d13b6a42eea37e13bf408eebb54
Martin LinuxVBox <marting12@live.dk>
2016-04-30 01:00:50 +0200

    Updates layout convenience

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+	# Finding users through current_user (defined by devise)
+	before_action :set_user, only: [:show]
+
+	def edit
+	end
+
   def show
-  	@user = User.find(params[:id])
-  	@all 	= User.all
+  	@all  = User.all
+  end
+
+  private
+
+  def set_user
+  	@user = User.find(current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :testpages
   devise_for :users
-  resources :users, :only => [:show]
+  resources :users, :only => [:show, :edit]
 
   devise_scope :user do
     authenticated  do
