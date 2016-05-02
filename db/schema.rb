f4daac4cfe94152588e2a8de71758b1cd604b83c
Martin LinuxVBox <marting12@live.dk>
2016-05-02 21:53:09 +0200

    Updates todo and edit user page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411163411) do
+ActiveRecord::Schema.define(version: 20160502161402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20160411163411) do
 
   add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
   add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "testpages", force: :cascade do |t|
     t.datetime "created_at", null: false
